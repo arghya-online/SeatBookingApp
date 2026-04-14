89b24d64ad9698678c9aad10192542c7a14ba72c
arghya-online <arghyaonline555@gmail.com>
2026-04-14 17:28:57 +0530

    render psql migrated

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,15 +5,20 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const isSslEnabled = process.env.DB_SSL === "true";
-
-const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "seat_booking_db",
-  password: process.env.DB_PASSWORD || "postgres",
-  port: Number(process.env.DB_PORT || 5433),
-  ssl: isSslEnabled ? { rejectUnauthorized: false } : false,
-});
+// If DATABASE_URL exists → use it (production)
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER || "postgres",
+      host: process.env.DB_HOST || "localhost",
+      database: process.env.DB_NAME || "seat_booking_db",
+      password: process.env.DB_PASSWORD || "postgres",
+      port: Number(process.env.DB_PORT || 5433),
+    });
 
 export default pool;
